04a78826eeed96becc5da05bf703a259ff1de289
fasyifa837 <fasyifa837@gmail.com>
2026-04-28 08:23:59 +0700

    Add files via upload

---
+++ src/config/db.js
@@ -0,0 +1,12 @@
+const { Pool } = require("pg");
+require("dotenv").config();
+
+const pool = new Pool({
+  host: "localhost",
+  port: 5432,
+  user: "postgres",
+  password: "bambang",
+  database: "auth_db",
+});
+
+module.exports = pool;
